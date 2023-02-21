e46b23a872898a4d22c4ad73a8af1e35a69bf49a
FirstGen <andy.nlarry@gmail.com>
2023-02-21 12:18:21 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_alphabet_x10();
+
+int main()
+{
+    for(int i=0; i<=9; i++){
+      void print_alphabet_x10();
+        printf("\n");
+    }
+    return 0;
+}
+
+void print_alphabet_x10(){
+    char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    printf(alphabet,"\n");
+    }
 
