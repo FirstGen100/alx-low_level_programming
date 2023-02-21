eeff3936d117cd5125fcbe41589dd225e4193df9
FirstGen <andy.nlarry@gmail.com>
2023-02-21 20:15:53 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* main - 
+* return: always 0
+*/
 int print_sign(int n);
-
 int main()
 {
     int r = -2;
     print_sign(r);
     return 0;
 }
-
+/**
+* print sign - 
+* return: always 0
+*/
 int print_sign(int n){
 
     if (n > 0){
