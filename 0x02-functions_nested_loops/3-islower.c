f973d4fbc5316a2e43b1b9c50fbf395357eeb5db
FirstGen <andy.nlarry@gmail.com>
2023-02-21 12:39:06 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int _islower(int c);
+
+int main()
+{
+    char r = 'c';
+    _islower(r);
+    return 0;
+}
+
+int _islower(int c){
+
+    if (c >= 'a' && c <= 'z'){
+        printf("1.\n");
+    }else{
+        printf("0.\n");
+        }
+
+    }
