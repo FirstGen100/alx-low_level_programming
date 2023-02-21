02f02f37f55efae8badfbb1cda822c8cea502c47
FirstGen <andy.nlarry@gmail.com>
2023-02-21 14:48:43 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void jack_bauer();
+
+
+int main()
+{
+    jack_bauer();
+
+    return 0;
+}
+
+void jack_bauer(){
+
+	for (int hour = 0; hour < 24; hour++){
+
+		for (int minute = 0; minute < 60; minute++){
+			printf("%d",hour/10);
+			printf("%d",hour%10);
+			printf(":");
+			printf("%d",minute/10);
+			printf("%d",minute%10);
+			printf("\n");
+		}
+	}
+}
