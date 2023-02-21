e5bd6904c47e910c52caaaaf642d580ee90471bc
FirstGen <andy.nlarry@gmail.com>
2023-02-21 13:03:42 +0300

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int print_sign(int n);
+
+int main()
+{
+    int r = -2;
+    print_sign(r);
+    return 0;
+}
+
+int print_sign(int n){
+
+    if (n > 0){
+        printf("+ \n");
+        return 1;
+    }else if (n == 0){
+        printf("0 \n");
+        return 0;
+        }
+    else{
+        printf("- \n");
+        return -1;
+    }
+
+    }
