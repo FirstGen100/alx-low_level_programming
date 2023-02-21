8f767b7d32f6e1d84a5af2b780ad0ff6ee2f3d02
FirstGen <andy.nlarry@gmail.com>
2023-02-21 11:23:10 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+int main(){
+  print_alphabet();
+  return 0;
+}
+
+void print_alphabet(){
+  char alphabet[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p.q.r.s.t.u.v.w.x.y.z};
+  printf(alphabet);
+}
+  
