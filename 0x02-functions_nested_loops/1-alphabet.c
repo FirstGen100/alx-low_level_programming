e3a69d1f068aaf9daf9478beb0869ba5c890ae46
FirstGen <andy.nlarry@gmail.com>
2023-02-21 19:21:15 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include "main.h"
-
-void print_alphabet();
-int main(){
-  print_alphabet();
-  return 0;
+/**
+ * main - Prints the letters of the alphabet
+ * return: 0 if successful
+*/
+int main(void)
+{
+void print_alphabet(void);
+print_alphabet();
+return 0;
 }
-
+/**
+ * print_alphabet - Prints the letters of the alphabet
+*/
 void print_alphabet(){
   char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
   printf(alphabet,"\n");
