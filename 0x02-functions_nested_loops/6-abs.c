956e666dee2803f64d06d761af95fe2e29a57264
FirstGen <andy.nlarry@gmail.com>
2023-02-21 13:38:23 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _abs(int);
+
+
+int main()
+{
+    int r;
+    r = _abs(-1);
+    printf("%d \n",r);
+    r = _abs(0);
+    printf("%d \n",r);
+    r = _abs(1);
+    printf("%d \n",r);
+    r = _abs(-98);
+    printf("%d \n",r);
+
+    return 0;
+}
+
+int _abs(int num){
+
+    if (num < 0){
+        num = (-1)*num;
+    }
+
+    return num;
+    }
