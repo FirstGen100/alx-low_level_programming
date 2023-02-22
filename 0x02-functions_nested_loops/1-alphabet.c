044d8566b6a08ce7812ce677add65120bbad860f
FirstGen <andy.nlarry@gmail.com>
2023-02-22 17:17:37 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -17,5 +17,5 @@ return 0;
 void print_alphabet()
 {
 char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-printf(alphabet,"\n");
+_putchar(alphabet);
 }
