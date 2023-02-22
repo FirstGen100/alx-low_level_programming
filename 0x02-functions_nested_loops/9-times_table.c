22620f7d9366ab74de3e9665442d5d882d611be2
FirstGen <andy.nlarry@gmail.com>
2023-02-22 13:27:51 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include "main.h"
 
+/*
+ * main - prints the times table of 9 starting with 0
+ * return: always 0
+ */
+void times_table(void);
+int main(void)
+{
+    times_table();
+
+    return 0;
+}
+/**
+* times_table - prints the times table of 9 starting with 0
+* return: 0 if successful
+*/
+void times_table(void)
+{
+	int rows,cols;
+
+	for (rows=0; rows<=9; rows++)
+    {
+        for (cols=0; cols<=9; cols++)
+        {
+            printf("%d, ",(rows*cols));
+        }
+        printf("\n");
+    }
+
+}
