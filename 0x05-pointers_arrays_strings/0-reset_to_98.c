d5eac656f0290bf7048ede99a376c5b99167a0af
FirstGen <andy.nlarry@gmail.com>
2023-02-28 17:22:34 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
-/*
- * main - prints the times table of 9 starting with 0
- * return: always 0
- */
-
-int n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return 0;
 /**
 * return_to_98 - prints the times table of 9 starting with 0
 * return: 0 if successful
