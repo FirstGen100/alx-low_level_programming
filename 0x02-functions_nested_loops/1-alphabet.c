7fe08537c2b697c5e0f2be4184009b3310246ed9
FirstGen <andy.nlarry@gmail.com>
2023-02-28 12:48:58 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,7 @@
  * main - Prints the letters of the alphabet
  * return: 0 if successful
 */
-void print_alphabet(void);
-int main(void)
-{
-print_alphabet();
-return 0;
-}
+
 /**
  * print_alphabet - Prints the letters of the alphabet
 */
