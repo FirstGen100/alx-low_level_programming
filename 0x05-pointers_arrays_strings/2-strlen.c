54b3741cbb21b663b5f3d06b2ecbf0fedbeba2fe
FirstGen <andy.nlarry@gmail.com>
2023-02-28 18:33:12 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - prints the length of a string
  * @s: the pointer to the string
- * return: the number of letters in the array(count)
+ * Return: the number of letters in the array(count)
 */
 int _strlen(char *s)
 {
@@ -12,6 +12,5 @@ for (count = 0; s[count] != '\0';)
 {
 count++;
 }
-printf("%d", count);
 return (count);
 }
