a4de35c83f92d3bf991e13cc3c56e92a1acca01e
FirstGen <andy.nlarry@gmail.com>
2023-02-28 18:14:05 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: the pointer to the string
+ * return: the number of letters in the array
+*/
+int _strlen(char *s)
+{
+int count;
+for (count = 0; &s[count] != '\0'; count++)
+{
+printf("%d", count);
+}
+return (count);
+}
