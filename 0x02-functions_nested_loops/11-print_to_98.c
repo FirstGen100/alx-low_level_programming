33682fc3e771afbae6296a9b7d3f605faa641e1c
FirstGen <andy.nlarry@gmail.com>
2023-02-28 16:26:26 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,5 @@
 #include "main.h"
-
-void print_to_98(int n);
-/**
- * main - Prints the letters of the alphabet
- * return: 0 if successful
-*/
-int main()
-{
-    print_to_98(0);
-    _putchar('\n');
-    print_to_98(98);
-    _putchar('\n');
-    print_to_98(111);
-    _putchar('\n');
-    print_to_98(81);
-    _putchar('\n');
-    print_to_98(-10);
-
-    return 0;
-}
+#include <stdio.h>
 /**
  * print_to_98 - Prints the numbers and ends at 98 
  * @n: the user variable
@@ -26,16 +7,23 @@ int main()
 */
 void print_to_98(int n)
 {
-	if (n < 98){
-        for(int x = n; x <= 98; x++){
-            _putchar(x + ',');
-        }
-	}else if(n > 98){
-        for(int y = n; y>=98; y--){
-           _putchar(y + ',');
-        }
-
-        }else{
-            _putchar(n);
-        }
+int x, y;
+if (n < 98)
+{
+for(int x = n; x <= 98; x++)
+{
+_putchar(x + ',');
+}
+}
+else if(n > 98)
+{
+for(int y = n; y>=98; y--)
+{
+_putchar(y + ',');
+}
+}
+else
+{
+_putchar(n);
+}
 }
