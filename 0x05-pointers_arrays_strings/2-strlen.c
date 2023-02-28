fc266d80cf06857605b660bcc04483d58a025bf0
FirstGen <andy.nlarry@gmail.com>
2023-02-28 18:30:18 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _strlen - prints the length of a string
  * @s: the pointer to the string
- * return: the number of letters in the array
+ * return: the number of letters in the array(count)
 */
 int _strlen(char *s)
 {
 int count;
-char word[] = &s;
-for (count = 0; word[count] != '\0'; count++)
+for (count = 0; s[count] != '\0';)
 {
-printf("%d", count);
+count++;
 }
+printf(count);
 return (count);
 }
