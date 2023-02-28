2cf5b1732f93eea3bc65859fba3d852577eb5918
FirstGen <andy.nlarry@gmail.com>
2023-02-28 14:41:25 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+1 \n");
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-printf("00 \n");
+_putchar('0');
 return (0);
 }
 else
 {
-printf("-1 \n");
+_putchar('-');
 return (-1);
 }
 }
