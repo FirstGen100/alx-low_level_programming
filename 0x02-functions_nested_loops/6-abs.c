38e21d329d348f9b30c6a228ca4121aefe0625f7
FirstGen <andy.nlarry@gmail.com>
2023-02-28 14:45:51 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,17 +3,17 @@
 /**
 * _abs - returns the absolute value of an iteger
 * @num: the number is converted to its absolute value
-* return: return the number's absolute value
+* Return: return the number's absolute value
 */
 int _abs(int num)
 {
-    if (num < 0)
-    {
-        num = (-1)*num;
-        return num;
-    }
-    else
-    {
-        return num;
-    }
+if (num < 0)
+{
+num = (-1)*num;
+return (num);
+}
+else
+{
+return (num);
+}
 }
