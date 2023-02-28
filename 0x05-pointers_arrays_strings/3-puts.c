bc6821ea4a707e0172aecb68dbf004fbc333109d
FirstGen <andy.nlarry@gmail.com>
2023-02-28 18:43:21 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - prints a string followed by a new line
+* @str: a pointer to the string variable
+* Return: always 0
+*/
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
