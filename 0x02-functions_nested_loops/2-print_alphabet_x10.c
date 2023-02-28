00754abde7dea3620e7bdb4e1c12085f800ff6f0
FirstGen <andy.nlarry@gmail.com>
2023-02-28 13:50:09 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,24 +4,27 @@
  * main - Prints the alphabet 10 times
  * return: Always 0 if success
 */
-void print_alphabet_x10(void);
 int main()
 {
-    int i;
-    for(i=0; i<=9; i++)
-    {
-        void print_alphabet_x10();
-        printf("\n");
-    }
-    return 0;
+int i;
+for (i=0; i<=9; i++)
+{
+void print_alphabet_x10();
+printf("\n");
+}
+return 0;
 }
 /**
- * main - Prints the alphabet 10 times
+ * print_alphabet_x10 - Prints the alphabet in lowercase
  * return: Always 0 if success
 */
 void print_alphabet_x10()
 {
-    char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    printf(alphabet,"\n");
+char alphabet;
+alphabet = 'a';
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
 }
-
+}
