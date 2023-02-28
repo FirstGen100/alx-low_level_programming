c65a03166cf41cad918a021b61b987604de21ee8
FirstGen <andy.nlarry@gmail.com>
2023-02-28 14:15:19 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,6 @@
 /**
  * _islower - Checks if there is a lower character
  * @c: the value to be tested
- * return: 1 if there is a lowercase letter
- * return 0 if there is no lowercase letter
 */
 int _islower(int c)
 {
