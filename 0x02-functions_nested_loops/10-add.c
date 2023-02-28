a2b73081a63a44be3ccb76ae02e217d99d7c590b
FirstGen <andy.nlarry@gmail.com>
2023-02-28 16:22:12 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int add(int, int);
-/**
- * main - Prints the letters of the alphabet
- * return: 0 if successful
-*/
-int main()
-{
-int n;
-n = add(89,9);
-printf("%d \n",n);
-
-return 0;
-}
+#include "main.h"
 /**
  * add - Adds two numbers
  *@x: first number
  *@y: second number
+ * Return: the sum of x and y
  */
 int add(int x, int y)
 {
