5cc510a6ca625b9c1d603169d440fead2f5028ac
FirstGen <andy.nlarry@gmail.com>
2023-02-28 14:23:44 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,17 @@
 #include "main.h"
 /**
 * _isalpha - checks if letter is in the aphabet
+* @c: the value to be tested
 * Return: 1 if in alphabet 0 otherwise
 */
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
