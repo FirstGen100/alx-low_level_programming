3e416758f3f7722ee579b38fbd3c6a707b15d68a
FirstGen <andy.nlarry@gmail.com>
2023-02-28 15:53:23 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
-* main - 
-* return: always 0
-*/
-void jack_bauer();
-int main()
-{
-    jack_bauer();
-
-    return 0;
-}
+#include "main.h"
 /**
 * jack_bauer - prints the time starting from 00:00 to 23:59
 * return: always 0
 */
 void jack_bauer()
 {
-	for (int hour = 0; hour < 24; hour++)
-	{
-		for (int minute = 0; minute < 60; minute++)
-		{
-			printf("%d",hour/10);
-			printf("%d",hour%10);
-			printf(":");
-			printf("%d",minute/10);
-			printf("%d",minute%10);
-			printf("\n");
-		}
-	}
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%d",hour/10);
+printf("%d",hour%10);
+printf(":");
+printf("%d",minute/10);
+printf("%d",minute%10);
+printf("\n");
+}
+}
 }
