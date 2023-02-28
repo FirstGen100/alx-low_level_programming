cf135aef2ee0f3db716aaea9065be3c775326284
FirstGen <andy.nlarry@gmail.com>
2023-02-28 14:35:14 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,23 +3,23 @@
 /**
 * print_sign - prints whether a number is positive, 0 or negative
 * @n: The number to be tested
-* return: always 0
+* Return: (1) if n greater than 0 (0) if equal to 0 and (-1) if less than 0
 */
 int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+ \n");
+printf("+, 1 \n");
 return (1);
 }
 else if (n == 0)
 {
-printf("0 \n");
+printf("0, 0 \n");
 return (0);
 }
 else
 {
-printf("- \n");
+printf("-, -1 \n");
 return (-1);
 }
 }
