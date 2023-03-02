55a266930d21dd86c83291e2f16df20971637283
FirstGen <andy.nlarry@gmail.com>
2023-03-02 14:11:44 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,4 +4,20 @@
 * print_rev - prints string in reverse
 * @s: pointer to the string
 */
-
+void print_rev(char *s)
+{
+int count, i, j;
+char rev[100];
+count=0;
+for(count = 0; s[count] != '\0';)
+{
+count++;
+}
+j = count - 1;
+for(i = 0; i < count; i++)
+{
+rev[i] = s[j];
+j--;
+}
+printf("%s", rev);
+}
