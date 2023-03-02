48dc24a52b0f987be6f902d07cac959acacbdba3
FirstGen <andy.nlarry@gmail.com>
2023-03-02 14:13:32 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,16 @@ void print_rev(char *s)
 {
 int count, i, j;
 char rev[100];
-count=0;
-for(count = 0; s[count] != '\0';)
+count = 0;
+for (count = 0; s[count] != '\0';)
 {
 count++;
 }
 j = count - 1;
-for(i = 0; i < count; i++)
+for (i = 0; i < count; i++)
 {
 rev[i] = s[j];
 j--;
 }
-printf("%s", rev);
+printf("%s\n", rev);
 }
