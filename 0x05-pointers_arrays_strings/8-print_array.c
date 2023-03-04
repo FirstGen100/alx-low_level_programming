e896fc3f3ed3fb9131e9b9b7dcf713594517d4a8
FirstGen <andy.nlarry@gmail.com>
2023-03-04 11:37:18 +0300

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - prints numbers in an array
+* @a: array to be tested
+* @n: the number of elements to be printed
+*/
+void print_array(int *a, int n)
+{
+int pos;
+for (pos = a[n]; a[pos]! = '\0'; pos++)
+{
+printf("%d, ", a[pos]);
+}
+}
