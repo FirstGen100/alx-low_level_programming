57e78e013d87dd7d26ec1b3f690554054b924838
FirstGen <andy.nlarry@gmail.com>
2023-03-04 09:52:25 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* puts_half - prints half a string
+* @str: string to be tested
+*
+*/
+void puts_half(char *str)
+{
+int count, check, i;
+
+for (count=0; str[count] != '\0'; count++);
+check = (count) % 2;
+if (check == 0)
+{
+for (i = (count) / 2; i <= count; i++)
+{
+printf("%c", str[i]);
+}
+}
+else
+{
+for (i = (count - 1) / 2; i <= count; i++)
+{
+printf("%c", str[i]);
+}
+}
+}
