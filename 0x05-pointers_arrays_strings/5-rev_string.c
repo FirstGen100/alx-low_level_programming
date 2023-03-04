b9b9dca3eef1c795d6f5d49c75eb116b6f908e82
FirstGen <andy.nlarry@gmail.com>
2023-03-04 08:43:43 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,23 +7,21 @@
 void print_rev(char *s)
 {
 char temp;
-
 int leng, g, count;
 count = 0;
-for(leng=0; s[leng] != '\0';)
+for(leng = 0; s[leng] != '\0';)
 {
-    leng++;
+leng++;
 }
-// iterate through each and every character of the string for printing it backwards or reverse direction
 
-for(g = (leng-1); g >= 0; g--)
+for(g = (leng - 1); g >= 0; g--)
 {
-    if(count < (leng)/2)
-    {
-        temp = s[count];
-        s[count] = s[g];
-        s[g] = temp;
-        count++;
-    }
+if(count < (leng) / 2)
+{
+temp = s[count];
+s[count] = s[g];
+s[g] = temp;
+count++;
+}
 }
 }
