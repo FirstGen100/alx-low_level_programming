0c8b43e5feb7367dfeebc7bdb7991405ed2cf2fa
FirstGen <andy.nlarry@gmail.com>
2023-03-04 09:04:22 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* puts2 -
+* @str: string to be tested
+*
+*/
+void puts2(char *str)
+{
+    int count;
+
+    for (count=0; str[count] != '\0'; count++)
+    {
+        if((str[count]) % 2 == 0)
+        {
+           printf("%c", str[count]);
+        }
+    }
+    printf("\n");
+}
