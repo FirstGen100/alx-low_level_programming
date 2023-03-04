704c25cd74f20c1279692af643380caee3724022
FirstGen <andy.nlarry@gmail.com>
2023-03-04 08:47:09 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,14 @@ void rev_string(char *s)
 char temp;
 int leng, g, count;
 count = 0;
-for(leng = 0; s[leng] != '\0';)
+for (leng = 0; s[leng] != '\0';)
 {
 leng++;
 }
 
-for(g = (leng - 1); g >= 0; g--)
+for (g = (leng - 1); g >= 0; g--)
 {
-if(count < (leng) / 2)
+if (count < (leng) / 2)
 {
 temp = s[count];
 s[count] = s[g];
