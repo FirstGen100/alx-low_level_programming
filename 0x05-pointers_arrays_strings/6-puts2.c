0b9fc9329e975a8eab386847febcd89d9a57951d
FirstGen <andy.nlarry@gmail.com>
2023-03-04 09:10:45 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* puts2 -
+* puts2 - prints a string
 * @str: string to be tested
 *
 */
 void puts2(char *str)
 {
-    int count;
+int count;
 
-    for (count=0; str[count] != '\0'; count++)
-    {
-        if((str[count]) % 2 == 0)
-        {
-           printf("%c", str[count]);
-        }
-    }
-    printf("\n");
+for (count = 0; str[count] != '\0'; count++)
+{
+if((str[count]) % 2 == 0)
+{
+printf("%c", str[count]);
+}
+}
+printf("\n");
 }
