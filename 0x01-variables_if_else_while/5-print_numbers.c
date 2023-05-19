ee735bc9eec48ad0ebcb51e26e3a113503cfb395
FirstGen <andy.nlarry@gmail.com>
2023-05-19 21:51:34 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
 /**
 * main - Print all single digit numbers
 *
-* Return: Always 0 (Success)
+* Return: 0 (Success)
 */
 int main(void)
 {
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
-	printf("%i", digit);
+	printf("%d", digit);
 	putchar('\n');
 
 	return (0);
