bc451372e6447e3d60f259f37a863aea9c336dfe
FirstGen <andy.nlarry@gmail.com>
2023-05-19 23:02:32 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,45 +1,34 @@
 #include <stdio.h>
 
 /**
-* main - Print all combinations of 3 digits
+* main - Print combinations of two digit numbers
 *
-*Return: 0 (success)
+* Return: 0 (Success)
 */
 int main(void)
 {
-int ones, tens, hundreds;
+int tens;
+int ones;
+int hundreds;
 
-for (ones = 0; ones < 8; ones++)
+for (tens = 0; tens <= 9; tens++)
 {
-for (tens = ones + 1; tens < 9; tens++)
+for (ones = tens + 1; ones <= 9; ones++)
 {
-for (hundreds = tens + 1; hundreds < 10; hundreds++)
+for (hundreds = ones + 1; hundreds <= 10 hundreds++)
 {
-// remove duplicate numbers
-if ((ones == 0 && tens == 1 && hundreds == 2) ||
-(ones == 1 && tens == 0 && hundreds == 2) ||
-(ones == 1 && tens == 2 && hundreds == 0) ||
-(ones == 2 && tens == 0 && hundreds == 1) ||
-(ones == 2 && tens == 1 && hundreds == 0))
-{
-continue;
-putchar(ones + '0');
 putchar(tens + '0');
+putchar(ones + '0');
 putchar(hundreds + '0');
-if (ones < 7 || tens < 8 || hundreds < 9)
+if (tens < 8)
 {
-putchar(',');
-putchar('');
+	putchar(',');
+	putchar(' ');
 }
 }
 }
 }
-
 putchar('\n');
- 
-return (0);
-}
 
- 
 return (0);
 }
