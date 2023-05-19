9992b390b2600ea349e9c6beb5cfc1081f275486
FirstGen <andy.nlarry@gmail.com>
2023-05-19 22:50:57 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,6 +8,34 @@
 int main(void)
 {
 int ones, tens, hundreds;
+
+for (ones = 0; ones < 8; ones++)
+{
+for (tens = ones + 1; tens < 9; tens++)
+{
+for (hundreds = tens + 1; hundreds < 10; hundreds++)
+{
+// remove duplicate numbers
+if ((ones == 0 && tens == 1 && hundreds == 2) || (ones == 1 && tens == 0 && hundreds == 2) ||
+(ones == 1 && tens == 2 && hundreds == 0) || (ones == 2 && tens == 0 && hundreds == 1) ||
+(ones == 2 && tens == 1 && hundreds == 0))
+continue
+putchar(ones + '0');
+putchar(tens + '0');
+putchar(hundreds + '0');
+if (ones < 7 || tens < 8 || hundreds < 9)
+{
+putchar(',');
+putchar('');
+}
+}
+}
+}
+putchar('\n');
+ 
+return (0);
+}
+
  
 return (0);
 }
