eaf2f338e4874892730606adac71c2a37a57aac8
FirstGen <andy.nlarry@gmail.com>
2023-05-20 09:12:05 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print all combinations of two two digit numbers
+*
+*Return: 0 (success)
+*/
+int main()
+{
+int i, j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+if ((i / 10 != j / 10) || (i % 10 != j % 10))
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if ((i < 98) || (j < 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
+}
