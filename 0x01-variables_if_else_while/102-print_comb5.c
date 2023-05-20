a86c51e342deaa371e3d987bbd7ec7c8f1ff0422
FirstGen <andy.nlarry@gmail.com>
2023-05-20 09:26:08 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int i, j;
 
 for (i = 0; i <= 99; i++)
 {
-for (j = i + 1; j < 99; j++)
+for (j = i + 1; j <= 99; j++)
 {
 if ((i / 10 != j / 10) || (i % 10 != j % 10))
 {
@@ -21,7 +21,7 @@ putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if ((i < 98) || (j < 99))
+if ((i < 98) || (j < 98))
 {
 putchar(',');
 putchar(' ');
