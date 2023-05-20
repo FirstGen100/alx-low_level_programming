16849b460148f49e514692e03a4629c65d7203e9
FirstGen <andy.nlarry@gmail.com>
2023-05-20 08:58:29 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int ones, tens, hundreds;
+/**
+* main - print all possible 3 digit combinations
+* 
+* Return: 0 (success)
+*/
+int main(void)
+{
+int ones, tens, hundreds;
 
-    for (ones = 0; ones <= 9; ones++)
-    {
-        for (tens = ones + 1; tens <= 9; tens++)
-        {
-            for (hundreds = tens + 1; hundreds <= 9; hundreds++)
-            {
-                if ((ones != tens && (ones != hundreds && (tens != hundreds))))
-                {
-                    putchar(ones + '0');
-                    putchar(tens + '0');
-                    putchar(hundreds + '0');
-                    if ((ones < 7) || ((ones == 7) && (tens < 8)) || ((ones == 8) && (tens == 9)))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
+for (ones = 0; ones <= 9; ones++)
+{
+for (tens = ones + 1; tens <= 9; tens++)
+{
+for (hundreds = tens + 1; hundreds <= 9; hundreds++)
+{
+if ((ones != tens && (ones != hundreds && (tens != hundreds))))
+{
+putchar(ones + '0');
+putchar(tens + '0');
+putchar(hundreds + '0');
+if ((ones < 7) || ((ones == 7) && (tens < 8)) || ((ones == 8) && (tens == 9)))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
