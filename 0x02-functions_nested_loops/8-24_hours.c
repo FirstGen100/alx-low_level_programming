d527dcd6f42354afc7d693bc39a9c1255731eedc
FirstGen <andy.nlarry@gmail.com>
2023-05-23 12:24:30 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 
 /**
 * jack_bauer - prints every minute of the day
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 int a, b, c, d;
 
-for (a = 0 a <= 2 a++)
+for (a = 0; a <= 2; a++)
 {
-for (b = 0 b <= 9 b++)
+for (b = 0; b <= 9; b++)
 {
-for (c = 0 c <= 5 c++)
+for (c = 0; c <= 5; c++)
 {
-for (d = 0 d <= 9 d++)
+for (d = 0; d <= 9; d++)
 {
-if (a >= 2 && b >= 4
+if (a >= 2; && b >= 4;)
 break;
 _putchar(a);
 _putchar(b);
