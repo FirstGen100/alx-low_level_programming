3848cd58ef0198f4589a2e0659047a58a2e0ad52
FirstGen <andy.nlarry@gmail.com>
2023-05-23 12:47:47 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-* main - prints _putchar
+* main - Print _putchar
 *
-* Return: 0 (success)
+* Return: 0 (Success)
 */
+
 int main(void)
 {
 int i;
-char c[] = "_putchar";
+char p[] = "_putchar";
 
 for (i = 0; i < 9; i++)
 {
-_putchar(c[i]);
+_putchar(p[i]);
 }
 _putchar('\n');
-
 return (0);
 }
