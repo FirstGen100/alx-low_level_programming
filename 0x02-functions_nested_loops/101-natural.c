d4d8a94a8b99ebefdf0b51dc3d5a67c86ff96327
FirstGen <andy.nlarry@gmail.com>
2023-05-23 11:51:23 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Prints natural numbers below 1024 that are multiplies of 3 or 5
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+int a, sum;
+
+for (a = 1; a < 1024; a++)
+{
+if ((a % 3) == 0 || (a % 5) == 0)
+sum += a;
+}
+printf("%d\n", sum);
+return (0);
+}
