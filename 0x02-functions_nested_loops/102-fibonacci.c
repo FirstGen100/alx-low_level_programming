0a194e89154eedf8260bb5e762f0455397003ee0
FirstGen <andy.nlarry@gmail.com>
2023-05-23 12:33:46 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,23 @@
 int main(void)
 {
 int n = 50, first = 1, second = 2, next, i;
-printf("%d, %d ", first, second);
 
-for (i = 2; i < n; i++)
-{
+printf("%d, %d, ", first, second);
+
+for (i = 2; i < n; i++) {
 next = first + second;
+if (i != n-1)
+{
+printf("%d, ", next);
+}
+else
+{
 printf("%d ", next);
+}
 first = second;
 second = next;
 }
-printf("\n");
 
+printf("\n");
 return (0);
 }
