a242001e230ddebad670ddd371c262993acb9b20
FirstGen <andy.nlarry@gmail.com>
2023-05-24 13:18:59 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+/**
+* main - prints the first 50 fibonacci numbers
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+long num1 = 1, num2 = 2, next, count, sum;
+
+printf("%ld, %ld, ", num1, num2);
+
+for (count = 3; count <= 50; count++)
+{
+next = num1 + num2;
+printf("%ld", next);
+
+if (next < 4000000)
+{
+sum = 2;
+if (next % 2 == 0)
+{
+sum += next;
+}
+else
+{
+break;
+}
+}
+num1 = num2;
+num2 = next;
+}
+printf("%ld\n", sum);
+
+return (0);
+}
