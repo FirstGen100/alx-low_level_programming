5ef5fd121bd00aed8b62a8124ac225527ae96b8d
FirstGen <andy.nlarry@gmail.com>
2023-05-24 13:25:04 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,23 +5,22 @@
 *
 * Return: 0 (success)
 */
-int main()
+int main(void)
 {
-    int prev_num = 1, curr_num = 2, next_num, total = 0;
-    
-    while (curr_num <= 4000000)
-    {
-        if (curr_num % 2 == 0)
-        {
-            total += curr_num;
-        }
-        
-        next_num = prev_num + curr_num;
-        prev_num = curr_num;
-        curr_num = next_num;
-    }
-    
-    printf("%d\n", total);
-    
-    return 0;
+int prev_num = 1, curr_num = 2, next_num, total = 0;
+
+while (curr_num <= 4000000)
+{
+if (curr_num % 2 == 0)
+{
+total += curr_num;
+}
+
+next_num = prev_num + curr_num;
+prev_num = curr_num;
+curr_num = next_num;
+}
+printf("%d\n", total);
+
+return 0;
 }
