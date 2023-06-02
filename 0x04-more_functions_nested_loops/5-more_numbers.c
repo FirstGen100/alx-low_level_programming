ad74260047613bdfd0d9c340ce0e7ae7ecf399d1
FirstGen <andy.nlarry@gmail.com>
2023-06-02 12:58:23 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,20 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
-* more_numbers - print 0-14 ten times
+* more_numbers - prints 10 times the numbers, from 0 to 14.
 */
-
 void more_numbers(void)
 {
-int i;
-char c;
+	int i;
+	char c;
 
-for (i = 0; i < 10; i++)
-{
-for (c = 0; c <= 14; c++)
-{
-if (c > 9)
-{
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-}
-_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
