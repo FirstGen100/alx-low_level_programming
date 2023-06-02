c6805700068284b0ce1ac04ef40c8f81913dd9eb
FirstGen <andy.nlarry@gmail.com>
2023-06-02 14:51:55 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include "main.h"
 /**
- *print_array - prints n elements of an array of integers, followed by a new line.
+ *print_array - prints n elements of an array of integers
  *@a: value
  *@n: value
  *Return: void
