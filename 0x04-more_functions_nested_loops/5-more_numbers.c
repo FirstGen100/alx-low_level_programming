67284b4cf01d3098fa68e0cfbd13687ef5390d1e
FirstGen <andy.nlarry@gmail.com>
2023-06-02 12:57:32 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,18 @@
 
 void more_numbers(void)
 {
+int i;
 char c;
-int count = 10;
 
-while (count != 0)
+for (i = 0; i < 10; i++)
 {
-for (c = '0'; c <= '14'; c++)
+for (c = 0; c <= 14; c++)
 {
-_putchar(c);
+if (c > 9)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
 _putchar('\n');
-count --;
 }
 }
