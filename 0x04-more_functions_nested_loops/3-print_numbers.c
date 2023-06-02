674a325a7054f7da561665bfa067ef1345c513c0
FirstGen <andy.nlarry@gmail.com>
2023-06-02 12:18:50 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0-9
+* Return: void
+*/
+
+void print_numbers(void)
+{
+int count;
+for (count = 0; count <= 9; count++)
+{
+_putchar(count);
+}
+_putchar('\n');
+}
