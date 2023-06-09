b263467f4ee9f2ffd2a7ec93d0f32b1cc4d95569
Andrew <andy.nlarry@gmail.com>
2023-06-09 15:26:34 +0300

    Update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - writes the name
+*@argc - arg count
+*@argv: array of pointer
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	argc = a
+
+
+
+
+
+
+
+
+	printf("%s\n", *argv);
+	return (0);
+}
