38fb4fb9f51d12194a4ced53055b0630e1daf160
FirstGen <andy.nlarry@gmail.com>
2023-06-09 15:49:11 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,30 +2,11 @@
 /**
 *main - start
 *@argc: number of args
-*argv: pointer to
-
-
-
-
-
-
-
-
-
-
-
-
-
+*argv: pointer to argument string
 *Return: 0
 */
 int main(int argc, char *argv[])
 {
-
-
-
-
-
-
 	while (argc--)
 		printf("%s\n", *argv++);
 	return (0);
