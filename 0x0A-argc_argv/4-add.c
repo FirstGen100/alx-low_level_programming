8723adaf88f3dc443abb2254e0eedf31c7d8c9b8
Andrew <andy.nlarry@gmail.com>
2023-06-09 16:10:13 +0300

    Update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*checker - check for characters
+*@a: char to be checked
+*Return: 0
+*/
+i
+
+
+
+
+
+
+
+
+int checker(char *a)
+{
+	int a, num, len;
+	
+	i = 0;
+	num
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
