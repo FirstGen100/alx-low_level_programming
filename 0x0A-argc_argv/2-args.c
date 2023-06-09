f21a45ec3913fcf6843fd21abb1be261b147dc6e
Andrew <andy.nlarry@gmail.com>
2023-06-09 15:44:31 +0300

    Update

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - start
+*@argc: number of args
+*argv: pointer to
+
+
+
+
+
+
+
+
+
+
+
+
+
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+
+
+
+
+
+
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
