fd91ab22c84e2f61632929d9e9ee2944291134d8
Andrew <andy.nlarry@gmail.com>
2023-06-09 16:38:11 +0300

    Update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - start
+*@argc: no of arguments
+*@argv: pointer to strings
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	return (0);
+}
