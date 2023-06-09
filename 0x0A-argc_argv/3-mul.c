c442a52e775f5e22db5b99793c86935fa97a7442
Andrew <andy.nlarry@gmail.com>
2023-06-09 15:57:31 +0300

    Update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - start
+*@argc:
+
+
+
+
+
+
+
+
+*@argv: pointer to arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv
+
+
+
+
+
+
+
+
+
+
+
+
+
