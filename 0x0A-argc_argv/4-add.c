d87c3b5cceaa7f358383fc6b82e11bd9795c7c12
FirstGen <andy.nlarry@gmail.com>
2023-06-09 16:22:25 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,39 +6,51 @@
 *@a: char to be checked
 *Return: 0
 */
-i
-
-
-
-
-
-
-
-
 int checker(char *a)
 {
 	int a, num, len;
 	
-	i = 0;
-	num
+	ii = 0;
+	num = 0;
+	len = strlen(a);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	while (i < len)
+	{
+		if (a[i] < '0' || a[i] > '9')
+		{
+			return (-1);
+			
+		}
+		else
+		{
+			num = num * 10 + (a[i] - '0');
+			
+		}
+		i++;
+	}
+	return (num);
+}
+/**
+*main - add numbers
+*@argc: argument count
+*@argv: array of pointers
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, num, sum;
+	sum = 0;
+	
+	for (i = 1; i < argc; i++)
+	{
+		num = checker(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
