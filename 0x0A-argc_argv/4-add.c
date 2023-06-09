d51c25e62c1429fe6b6f90943ea421cabd7df9d8
Andrew <andy.nlarry@gmail.com>
2023-06-09 16:27:52 +0300

    Update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,50 +1,48 @@
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /**
-*checker - check for characters
-*@a: char to be checked
-*Return: 0
-*/
-int checker(char *a)
+ * num_checker - checks if a given char is number or not
+ * @a: char to be checked
+ * Return: 1, if its a number, else 0
+ **/
+int num_checker(char *a)
 {
-	int a, num, len;
+	int i, num, len;
 
-	ii = 0;
+	i = 0;
 	num = 0;
 	len = strlen(a);
-
 	while (i < len)
 	{
 		if (a[i] < '0' || a[i] > '9')
 		{
 			return (-1);
-
 		}
 		else
-		{
 			num = num * 10 + (a[i] - '0');
-
-		}
 		i++;
 	}
 	return (num);
 }
 /**
-*main - add numbers
-*@argc: argument count
-*@argv: array of pointers
-*Return: 0
-*/
+ * main - add positive numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of addition or 1
+ **/
 int main(int argc, char *argv[])
 {
 	int i, num, sum;
 
 	sum = 0;
-
 	for (i = 1; i < argc; i++)
 	{
-		num = checker(argv[i]);
+		num = num_checker(argv[i]);
 		if (num == -1)
 		{
 			printf("Error\n");
