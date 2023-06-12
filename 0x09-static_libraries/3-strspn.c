3dcfd487f0d42681a759c962328dc3d247843ad9
Andrew <andy.nlarry@gmail.com>
2023-06-12 09:58:40 +0300

    Update

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,1 +1,21 @@
 #include "main.h"
+[H[H[H[H[H[H[H[H[H[H[H[H[H
+/**
+*_strspn - emptyfunction
+* @s: first variable
+* @accept: accepted variable
+* Return: void
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+}
+
+/**
+*_strspn - emptyfunction
+* @s: first variable
+* @accept: accepted variable
+* Return: void
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+}
