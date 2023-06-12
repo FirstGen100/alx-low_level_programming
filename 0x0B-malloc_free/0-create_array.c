7d21519c51094af3987483b01f38afff81d86c9e
FirstGen <andy.nlarry@gmail.com>
2023-06-12 15:31:19 +0300

    Update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 /**
  * *create_array - creates an array of chars, and initializes it with a char
  * @size: size of the array
- * @c: char to be initialized with
+ * @c: char to be initialized
  * Return: pointer to array or null
  **/
 char *create_array(unsigned int size, char c)
