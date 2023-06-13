8392bef6393f968c959e734949c3b5ef4682a9bb
Andrew <andy.nlarry@gmail.com>
2023-06-13 12:22:49 +0300

    Update

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+size_t strspn(const char *s, const char *accept)
+{
+    const char *p;
+    const char *a;
+    size_t count = 0;
+
+    for (p = s; *p != '\0'; p++) {
+        for (a = accept; *a != '\0'; a++) {
+            if (*p == *a) {
+                count++;
+                break;
+            }
+        }
+        if (*a == '\0') {
+            return count;
+        }
+    }
+
+    return count;
+}
