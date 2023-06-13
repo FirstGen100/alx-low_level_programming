a5c8a4f2a828a07f5d48be4d329b8362fe458096
Andrew <andy.nlarry@gmail.com>
2023-06-13 12:07:08 +0300

    Update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,7 @@
 #include "main.h"
 #include <unistd.h>
-/**
-* _putchar - write to stdout
-* @c: character
-* Return: char
-*/
-char _putchar(char c)
+
+int _putchar(char c)
 {
-	return write(1, &c, 1)
+    return (write(1, &c, 1));
 }
