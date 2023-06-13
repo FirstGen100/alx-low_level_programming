d4546ad46b9c9abaa390bd90595fa43ad3034be7
Andrew <andy.nlarry@gmail.com>
2023-06-13 11:50:23 +0300

    Update

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* atoi - function
+* @s: character
+* Return: char
+*/
+[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H[H
+int _atoi(char *s)
+{
+}
