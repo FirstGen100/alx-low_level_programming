faa115152b2518356b6bef23a91d21482a599798
FirstGen <andy.nlarry@gmail.com>
2023-06-15 12:38:38 +0300

    Update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size = (len + n) * sizeof(*ptr);
 	ptr = malloc(size + 1);
 	if (ptr == NULL)
-		return(NULL);
+		return (NULL);
 	i = 0;
 	while (i < size && s1[i] != '\0')
 	{
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	j = 0;
-	while (i < size && s2 [j] != '\0')
+	while (i < size && s2[j] != '\0')
 	{
 		ptr[i] = s2[j];
 		i++;
