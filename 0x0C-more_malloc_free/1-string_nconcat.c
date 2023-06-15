2e74e80531a759df06a793693d127f1486ba14b3
Andrew <andy.nlarry@gmail.com>
2023-06-15 10:56:34 +0300

    Update

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* string_nconcat - 
+* @s1: destination
+* @s2: source of second string
+* @n: size of
+
+
+
+
+
+
+
+
+
+
+*/
+/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int count, count1;
+	int sign = n;
+	char *ptr;
+	int len1, len2;
+	return (ptr);
+}
