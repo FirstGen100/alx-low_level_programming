52f34e49ea2b453d17b31527feadb3e1d631ae5b
Andrew <andy.nlarry@gmail.com>
2023-06-15 13:49:17 +0300

    Update

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - multiply two numbers
+* @ argc: number of args
+* @argv: arguments
+* Return
+
+
+
+
+
+
+
+
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, carry, len1, len2, len_res, res_
+	int *res;
+	char *num1, *num2;
+
+	if(argc != 3)
+	{
+		printf("Error\n
+
+
+
+
+
+
+
+
