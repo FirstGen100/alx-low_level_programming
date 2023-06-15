686e8387df828f609543487c4093748a0f09ebb1
Andrew <andy.nlarry@gmail.com>
2023-06-15 12:26:50 +0300

    Update

---
+++ 0x0C-more_malloc_free/calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - allocate memory for an array of elements
+* @nmemb: number of elements
+* @size: size of elements
+* Return: pointer to memory
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsignes int ptrsize, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptrsize = nmemb * size;
+	ptr = malloc(ptrsize);
+	if(ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < ptrsize)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
