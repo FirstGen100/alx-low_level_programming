77e9baea9acc0d82dd9da3df0bb26d00f12d96bb
FirstGen <andy.nlarry@gmail.com>
2023-06-15 10:48:33 +0300

    Update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 
 /**
 * malloc_checked - allocates memory using malloc
-* @b: unsigned int variable 
+* @b: unsigned int variable
 * Return: pointer to the allocated memory
 */
 void *malloc_checked(unsigned int b)
