b74812f50bd37de4c759ddcc3c1505a7093a2cde
Andrew <andy.nlarry@gmail.com>
2023-06-27 20:21:09 +0300

    Update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,9 +7,11 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
- * struct typ - structure 
+ * struct type - structure
  * @format: character
  * @function: associated function
+ *
+ * Description: structure has the format and associated functions
  */
 typedef struct type
 {
