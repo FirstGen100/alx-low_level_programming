c9e689a489a6fe160056fde685b0ff5eb3a57b60
Andrew <andy.nlarry@gmail.com>
2023-07-01 03:16:44 +0300

    Update

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**free_list - frees a list
+ * @head: start of list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	for (; head;)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
