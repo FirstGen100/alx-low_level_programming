67d22396a14676a60a28a5559514ee63f9f043f1
Andrew <andy.nlarry@gmail.com>
2023-07-12 09:24:41 +0300

    Update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN
 #define MAIN
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
