0382d51fa6c26c75c8cf661e1222db9cd4ea4d24
Andrew <andy.nlarry@gmail.com>
2023-08-04 22:08:09 +0300

    Update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,14 +8,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flipper = 0;
-	unsigned int count;
+	int i, count;
 
-	for (count = 8 * sizeof(n) - 1; count > 0; count--)
-	{
-		if (((n ^ m) >> count) & 1)
-			flipper++;
-	}
-	return (flipper);
+	count = 0;
+
+	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
+			if (((n ^ m) >> i) & 1)
+				count++;
+	return (count);
 }
 
