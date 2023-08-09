d48931ce01c3ff58ec8f4e5d8c88866cf638e1ad
Andrew <andy.nlarry@gmail.com>
2023-08-09 23:50:33 +0300

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,36 +7,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t f_read, f_write, count = 0;
-	FILE *directory;
+	int f_read, f_write;
+	int directory;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	directory = fopen(filename, "r");
-	if (directory == NULL)
+	directory = open(filename, O_RDONLY);
+	if (directory < 0)
 		return (0);
-	buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		fclose(directory);
 		return (0);
 	}
-	while ((f_read = fread(buffer, sizeof(char), letters, directory)) > 0)
+	f_read = read(directory, buffer, letters);
+	if (f_read < 0)
 	{
-		buffer[f_read] = '\0';
-		f_write = write(STDOUT_FILENO, buffer, f_read);
-		if (f_write < 0 || f_read != f_write)
-		{
-			fclose(directory);
-			free(buffer);
-			return (0);
-		}
-		count += f_read;
+		free(buffer);
+		return (0);
+	}
+	f_write = write(STDOUT_FILENO, buffer, f_read);
+	if (f_write < 0 || f_read != f_write)
+	{
+		free(buffer);
+		return (0);
 	}
 	free(buffer);
-	fclose(directory);
-	return (count);
+	close(directory);
+	return (f_write);
 }
 
