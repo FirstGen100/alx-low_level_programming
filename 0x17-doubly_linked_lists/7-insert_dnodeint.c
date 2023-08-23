ac7acb7d81d2d9082ca1f83ca5b537a472a5dbb7
Andrew <andy.nlarry@gmail.com>
2023-08-23 15:44:25 +0300

    Update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n, new_node->prev = new_node->next = NULL;
-
 	if (idx == 0)
 	{
 		if (*head)
